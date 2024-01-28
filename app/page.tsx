67fae2572845a5c12b8431fbbb27a66fe290a7af
joshBreite <joshuabreite2022@u.northwestern.edu>
2024-01-28 12:57:21 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'zizzamia.xyz',
     description: 'LFG',
-    images: ['https://zizzamia.xyz/park-1.png'],
+    images: ['https://zizzamia.xyz/park-1.png', "https://pbs.twimg.com/profile_images/1747384497635516416/ssEcaH32_400x400.png"],
   },
   other: {
     ...frameMetadata,
