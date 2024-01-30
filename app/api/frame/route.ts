2ec7dbf7444c39b6deb818aea40e4271b080fa25
joshBreite <joshuabreite2022@u.northwestern.edu>
2024-01-30 17:51:59 -0500

    Added Iceberg Images

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -12,7 +12,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="park-1.png" />
+    <meta property="fc:frame:image" content="iceberg-option-1.png" />
     <meta property="fc:frame:button:1" content="${accountAddress}" />
     <meta property="fc:frame:post_url" content="https://zizzamia.xyz/api/frame" />
   </head></html>`);
