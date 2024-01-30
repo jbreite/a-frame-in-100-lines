747916c3a3795960069999ead68e750a4b26b5a0
joshBreite <joshuabreite2022@u.northwestern.edu>
2024-01-30 18:51:01 -0500

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,20 +1,29 @@
 import { getFrameAccountAddress } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 
+let counter = 1; // Initialize the counter
+
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  let accountAddress = '';
-  try {
-    const body: { trustedData?: { messageBytes?: string } } = await req.json();
-    accountAddress = await getFrameAccountAddress(body, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
-  } catch (err) {
-    console.error(err);
-  }
+  // let accountAddress = '';
+  // try {
+  //   const body: { trustedData?: { messageBytes?: string } } = await req.json();
+  //   accountAddress = await getFrameAccountAddress(body, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
+  // } catch (err) {
+  //   console.error(err);
+  // }
+  const postUrl = 'https://a-frame-in-100-lines-jbreite.vercel.app/api/frame';
+
+  const baseImageUrl = 'https://a-frame-in-100-lines-jbreite.vercel.app/iceberg-option-';
+  let optionImageUrl = `${baseImageUrl}${counter}.png`;
+
+  counter = counter >= 4 ? 1 : counter + 1;
+
 
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://a-frame-in-100-lines-jbreite.vercel.app/iceberg-option-2.png" />
-    <meta property="fc:frame:button:1" content="${accountAddress}" />
-    <meta property="fc:frame:post_url" content="https://a-frame-in-100-lines-jbreite.vercel.app/api/frame" />
+    <meta property="fc:frame:image" content="${optionImageUrl}" />
+    <meta property="fc:frame:button:1" content="Next" />
+    <meta property="fc:frame:post_url" content="${postUrl}" />
   </head></html>`);
 }
 
