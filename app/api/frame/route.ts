4ab0be2dc16a1f30d2f0ed383546cd50680edd6f
joshBreite <joshuabreite2022@u.northwestern.edu>
2024-01-30 19:34:57 -0500

    Testing new format

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,22 +1,48 @@
-import { getFrameAccountAddress } from '@coinbase/onchainkit';
+import { getFrameAccountAddress, getFrameMessage } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 
 let counter = 1; // Initialize the counter
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  // let accountAddress = '';
-  // try {
-  //   const body: { trustedData?: { messageBytes?: string } } = await req.json();
-  //   accountAddress = await getFrameAccountAddress(body, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
-  // } catch (err) {
-  //   console.error(err);
-  // }
   const postUrl = 'https://a-frame-in-100-lines-jbreite.vercel.app/api/frame';
 
   const baseImageUrl = 'https://a-frame-in-100-lines-jbreite.vercel.app/iceberg-option-';
   let optionImageUrl = `${baseImageUrl}${counter}.png`;
 
   counter = counter >= 4 ? 1 : counter + 1;
+
+
+  let accountAddress: string | undefined;
+  let messageBytes: string | undefined;
+  try {
+    const body = await req.json();
+    const validatedMessage = await getFrameMessage(body)
+    console.log('validatedMessage', validatedMessage);
+    accountAddress = await getFrameAccountAddress(body, {
+      NEYNAR_API_KEY: process.env.NEYNAR_API_KEY,
+    });
+  } catch (err) {
+    console.error(err);
+    return new NextResponse(`<!DOCTYPE html><html><head>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="${optionImageUrl}" />
+    <meta property="fc:frame:button:1" content="Next" />
+    <meta property="fc:frame:post_url" content="${postUrl}" />
+  </head></html>`);
+  }
+  console.log('Message is valid');
+
+  if (!accountAddress) {
+    return new NextResponse(`<!DOCTYPE html><html><head>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="${optionImageUrl}" />
+    <meta property="fc:frame:button:1" content="Next" />
+    <meta property="fc:frame:post_url" content="${postUrl}" />
+  </head></html>`);
+  }
+
+  console.log('Account address is', accountAddress);
+    
 
 
   return new NextResponse(`<!DOCTYPE html><html><head>
