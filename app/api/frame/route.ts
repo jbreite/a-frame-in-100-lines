dfebb0ed050540bf88e1e74089835dda211eb214
joshBreite <joshuabreite2022@u.northwestern.edu>
2024-01-30 21:16:28 -0500

    Got rid of dynamic

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -77,7 +77,7 @@ export default async function handler(req: NextRequest): Promise<Response> {
   return getResponse(req);
 }
 
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 
 
 // let counter = 1; // Initialize the counter
